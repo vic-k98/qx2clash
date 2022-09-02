5055984283554a3048452536248227aa9d7d149f
Vic Guo <vicg997@gmail.com>
2022-09-03 04:14:22 +0800

    1

--- config/index.js
+++ config/index.js
@@ -1,6 +1,6 @@
 const backend = 'http://192.168.31.99:25500/sub';
-const configUrl = 'http://192.168.31.99:5500/qx2clash_bf/temp/custom_config.ini';
-const excludeKey = '剩余|过期|官网|官方|更新';
+const configUrl = 'http://192.168.31.99:5500/qx2clash/temp/custom_config.ini';
+const excludeKey = '剩余|过期|官网|官方|更新|到期';
 
 const rulesReplacePrefix = 'http://192.168.31.99:5500/ios_rule_script/';
 const ruleReplaceReg = /^(.+)rule\/QuantumultX(.+)$/;
