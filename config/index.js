96b7380bae94211c5523d08e71cc9c1432e34726
Vic Guo <vicg997@gmail.com>
2022-09-03 03:27:37 +0800

    完成服务逻辑

--- config/index.js
+++ config/index.js
@@ -5,7 +5,7 @@ const excludeKey = '剩余|过期|官网|官方|更新';
 const rulesReplacePrefix = 'http://192.168.31.99:5500/ios_rule_script/';
 const ruleReplaceReg = /^(.+)rule\/QuantumultX(.+)$/;
 
-const localSubUrl = 'http://192.168.31.99:5500/qx2clash_bf/temp/local-sub.yaml';
+const localSubUrl = 'http://192.168.31.99:3000/clash/local-sub.yaml';
 
 module.exports = {
   backend,
